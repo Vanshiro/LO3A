6cc938dba8ea396fe90748c1c59914936ad7571d
Your New Name <vann7568@gmail.com>
2026-01-22 13:40:25 -0700

    Part 1

--- Detective.java
+++ Detective.java
@@ -1,3 +1,8 @@
 public class Detective {
-
+    String name;
+    int badgeNum;
+    public Detective(String name, int badgeNum){
+        this.name = name;
+        this.badgeNum = badgeNum;
+    }
 }
--- PublicSafety.java
+++ PublicSafety.java
@@ -1,3 +1,19 @@
 public class PublicSafety {
+    Station campus;
+    Station city;
+    int badgeNum;
+    public PublicSafety(String campus, String city){
+        this.campus = new Station(campus);
+        this.city = new Station(city);
+        badgeNum = 0;
+    }
+    public void doHire(boolean hire){
+        badgeNum ++;
+        if(hire){
+            city.hireDetective(badgeNum);
+        }else{
+            campus.hireDetective(badgeNum);
+        }
+    }
 
 }
